3891f698d07a359f3b9aca9cb4420f81e4375e15
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-10-29 13:04:14 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -91,19 +91,3 @@ with columns[1]:
     #st.sidebar.info('update to GoogleSheets')
    
 
-# Create a connection object.
-conn = connect()
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
