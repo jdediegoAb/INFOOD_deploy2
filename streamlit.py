21addfb261ecbb6287a6ce9a282ddaefe3596fd8
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-10-29 13:05:06 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -67,7 +67,6 @@ with columns[1]:
 
     # Perform SQL query on the Google Sheet.
     # Uses st.cache to only rerun when the query changes or after 10 min.
-    @st.cache(ttl=600)
     def run_query(query):
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
