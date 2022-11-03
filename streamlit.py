1a2bd98f11e02cc0adf97e97320334469d74e73d
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-11-03 11:10:24 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -63,7 +63,7 @@ with columns[1]:
 
 # Create a connection object.
 #sheet_url = st.secrets["public_gsheets_url"]
-scope=["https://www.googleapis.com/auth/drive.file"]#,'https://www.googleapis.com/auth/spreadsheets',,"https://www.googleapis.com/auth/drive"]
+scope=["https://www.googleapis.com/auth/drive"]#,'https://www.googleapis.com/auth/spreadsheets',]
 
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
