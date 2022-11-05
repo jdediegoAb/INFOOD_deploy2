3e80c895fede4a47de36359d9817b5f4195287a9
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-11-05 11:08:49 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -111,10 +111,6 @@ with columns[1]:
         rows = rows.fetchall()
         return rows
     query= f'SELECT input COUNT(input) AS input_count FROM "{sheet_url}" GROUP BY "input" ORDER by "input_count" LIMIT 3'
-    SELECT my_column COUNT(my_column)
-
-
-    
     rows = run_query(query)
     # Print results.
     st.table(rows)
