d6135767c7e689e618a802330183eb0396c8b3bd
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-11-05 10:45:23 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -110,7 +110,7 @@ with columns[1]:
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
         return rows
-    query= f'SELECT * FROM "{sheet_url}"'
+    query= f'SELECT * FROM "{sheet_url}" count (input)'
     rows = run_query(query)
     # Print results.
     st.table(rows)
