6fa08d752803d1d76b27dd2c6fd3d015a84300fe
jdediegoAb <102971120+jdediegoAb@users.noreply.github.com>
2022-11-05 10:38:37 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -110,7 +110,7 @@ with columns[1]:
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
         return rows
-    query="SELECT *, count(input) FROM "{sheet_url}" GROUP BY input ORDER BY count(input) DESC LIMIT 3 "
+    query="SELECT *, count(input), FROM "{sheet_url}", GROUP BY input, ORDER BY count(input), DESC LIMIT 3 "
     rows = run_query(query)
     # Print results.
     st.table(rows)
